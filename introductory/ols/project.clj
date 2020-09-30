deac4042eb02c87f5a78cc74f7a548b8bde244d2
alex314159 <alex314159@users.noreply.github.com>
2020-09-30 20:58:36 +0100

    bump tech.ml and tablecloth versions

--- introductory/ols/project.clj
+++ introductory/ols/project.clj
@@ -1,4 +1,4 @@
-(defproject ols "0.1.0-SNAPSHOT"
+(defproject ols "0.1.1-SNAPSHOT"
   :description "Simple OLS tutorial"
   :url ""
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -7,7 +7,7 @@
                  ;[org.clojars.haifengl/smile "2.5.1"] Java interop below seems more complete at least re documentation
                  [com.github.haifengl/smile-core "2.5.1"]
                  [com.github.haifengl/smile-mkl "2.5.1"]    ;necessary unless MKL is in your path
-                 [techascent/tech.ml.dataset "4.01"]
-                 [scicloj/tablecloth "1.0.0-pre-alpha9"]
+                 [techascent/tech.ml.dataset "4.04"]
+                 [scicloj/tablecloth "4.04"]
                  ]
   :repl-options {:init-ns ols.core})
