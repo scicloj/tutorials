ecd24e59cf96bb71428021f6ff76e9201e58b50c
alex314159 <alex314159@users.noreply.github.com>
2020-12-12 18:09:42 +0000

    update to tech.ml.data 5.00 and smile 2.5.3

--- introductory/ols/project.clj
+++ introductory/ols/project.clj
@@ -1,13 +1,13 @@
-(defproject ols "0.1.1-SNAPSHOT"
+(defproject ols "0.1.2-SNAPSHOT"
   :description "Simple OLS tutorial"
   :url ""
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 ;[org.clojars.haifengl/smile "2.5.1"] Java interop below seems more complete at least re documentation
-                 [com.github.haifengl/smile-core "2.5.1"]
-                 [com.github.haifengl/smile-mkl "2.5.1"]    ;necessary unless MKL is in your path
-                 [techascent/tech.ml.dataset "4.04"]
-                 [scicloj/tablecloth "4.04"]
-                 ]
+                 [com.github.haifengl/smile-core "2.5.3"]
+                 [com.github.haifengl/smile-mkl "2.5.3"]
+                 ;[org.clojars.haifengl/smile "2.5.3"]; not necessary
+                 ;[org.bytedeco/arpack-ng "3.7.0-1.5.3"];can't get this to work
+
+                 [techascent/tech.ml.dataset "5.00-beta-14"]]
   :repl-options {:init-ns ols.core})
